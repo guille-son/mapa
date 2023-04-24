1d42a18177f171184222d490810a8fe1ce11d6d2
guille-son <guillermoaguinaga1998@gmail.com>
2023-04-24 10:43:57 -0600

    Commit 1 desde VS

--- js/modal.js
+++ js/modal.js
@@ -74,6 +74,118 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $("#Wiwilli_De_Jinotegabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#El_Cuabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Santa_Maria_De_Pantasmabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Jinotegabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_Sebastian_De_Yalibtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_Rafael_Del_Nortebtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#La_Concordiabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+//madriz
+
+$(document).ready(function(){
+    $("#Telpanecabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_Juan_de_Río_Cocobtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Totogalpabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Palacaguinabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Yalaguinabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_José_de_Cusmapabtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Somotobtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_Lucasbtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#Las_Sabanasbtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+// Esteli
+
+$(document).ready(function(){
+    $("#Estelibtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#San_Juan_De_Limaybtn").click(function(){
+        $("#modal").modal('show');
+    });
+});
+
 
 
 
